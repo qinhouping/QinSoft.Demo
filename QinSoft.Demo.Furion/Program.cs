cd97d08be642552f3b90b2df5ed926b62083e790
qinhouping <877795526@qq.com>
2023-10-09 21:51:21 +0800

    增加furion案例

---
+++ QinSoft.Demo.Furion/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Furion.Components;
+
+Serve.Run(RunOptions.Default.WithArgs(args).ConfigureServices(services =>
+{
+    services.AddFileConfiger(options =>
+    {
+        options.ExpireIn = 600;
+    });
+    services.AddDatabaseManager(options =>
+    {
+
+    });
+
+    services.AddServices();
+    services.AddMappers();
+    services.AddRepositories();
+
+}).AddComponent<ServeServiceComponent>().UseComponent<ServeApplicationComponent>());
---
+++ QinSoft.Demo.Furion/Services/TestServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QinSoft.Demo.BLL.Services;
+using QinSoft.Demo.Common.Model.Response;
+
+namespace QinSoft.Demo.Furion.Services
+{
+    [DynamicApiController]
+    public class TestService
+    {
+        private ITestService testService;
+
+        public TestService(ITestService testService)
+        {
+            this.testService = testService;
+        }
+
+        [HttpGet]
+        public List<Project> Get()
+        {
+            return testService.GetProjects();
+        }
+    }
+}
